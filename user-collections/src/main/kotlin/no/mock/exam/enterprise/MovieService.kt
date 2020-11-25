3b5a89e700d61619efd5377dbbda9ef639ae933f
OpKris <kristofferopdahl@mail.com>
2020-11-25 20:39:04 +0100

    getRandomSelection() -> MovieService.kt

--- user-collections/src/main/kotlin/no/mock/exam/enterprise/MovieService.kt
+++ user-collections/src/main/kotlin/no/mock/exam/enterprise/MovieService.kt
@@ -1,31 +1,33 @@
 package no.mock.exam.enterprise
 
+import no.mock.exam.enterprise.dto.Hall
 import no.mock.exam.enterprise.model.Movie
 import no.mock.exam.enterprise.model.Collection
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.lang.IllegalArgumentException
 import javax.annotation.PostConstruct
+import kotlin.random.Random
 
 @Service
 class MovieService {
 
-    companion object{
+    companion object {
         private val logger = LoggerFactory.getLogger(MovieService::class.java)
     }
 
     protected var collection: Collection? = null
 
-    val movieCollection : List<Movie>
-    get() = collection?.movies ?: listOf()
+    val movieCollection: List<Movie>
+        get() = collection?.movies ?: listOf()
 
     private val lock = Any()
 
     @PostConstruct
-    fun init(){
+    fun init() {
 
-        synchronized(lock){
-            if(movieCollection.isNotEmpty()){
+        synchronized(lock) {
+            if (movieCollection.isNotEmpty()) {
                 return
             }
             fetchData()
@@ -34,26 +36,57 @@ class MovieService {
 
     fun isInitialized() = movieCollection.isEmpty()
 
-    protected fun fetchData(){
+    protected fun fetchData() {
         // TODO
     }
 
-    private fun verifyCollection(){
+    private fun verifyCollection() {
 
-        if(collection == null){
+        if (collection == null) {
             fetchData()
 
-            if(collection == null){
+            if (collection == null) {
                 throw IllegalArgumentException("No Collection information")
             }
         }
     }
 
-    fun price(movieId: String) : Int {
+    fun price(movieId: String): Int {
         verifyCollection()
-        val movie : Movie = movieCollection.find { it.movieId == movieId} ?:
-                throw IllegalArgumentException("Invalid movieId $movieId")
+        val movie: Movie = movieCollection.find { it.movieId == movieId }
+                ?: throw IllegalArgumentException("Invalid movieId $movieId")
 
         return collection!!.prices[movie.hall]!!
     }
-}
+
+    fun getRandomSelection(n: Int): List<Movie> {
+
+        if (n <= 0) {
+            throw IllegalArgumentException("Non-positive n: $n")
+        }
+
+        verifyCollection()
+
+        val selection = mutableListOf<Movie>()
+
+        val probabilities = collection!!.hallProbabilities
+        val small = probabilities[Hall.SMALL]!!
+        val medium = probabilities[Hall.MEDIUM]!!
+        val large = probabilities[Hall.LARGE]!!
+        val fourDX = probabilities[Hall.FourDX]!!
+
+        repeat(n){
+            val p = Math.random()
+            val r = when {
+                p <= small -> Hall.SMALL
+                p > small && p <= small + medium -> Hall.MEDIUM
+                p > small + medium && p <= small + medium + large -> Hall.LARGE
+                p > small + medium + large -> Hall.FourDX
+                else -> throw IllegalStateException("BUG for p=$p")
+            }
+            val movie = collection!!.moviesByHall[r].let{ it!![Random.nextInt(it.size)]}
+        }
+        return selection
+
+    }
+} // end class MovieService
