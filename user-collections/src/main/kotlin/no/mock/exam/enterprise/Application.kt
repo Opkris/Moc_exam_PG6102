e3a226445f9da55debb021fa44d5d2c0910b8bfb
OpKris <kristofferopdahl@mail.com>
2020-11-25 11:41:20 +0100

    created Application.kt with ApiInfo

--- user-collections/src/main/kotlin/no/mock/exam/enterprise/Application.kt
+++ user-collections/src/main/kotlin/no/mock/exam/enterprise/Application.kt
@@ -1,2 +1,24 @@
 package no.mock.exam.enterprise
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.service.ApiInfo
+
+
+@SpringBootApplication
+class Application {
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("API for User-Collections")
+                .description("REST service to handle the movie bought.")
+                .version("1.0")
+                .build()
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
