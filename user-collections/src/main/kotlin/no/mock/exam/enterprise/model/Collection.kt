4b02243a178cf05e43d036a56fefc6b62919d31a
OpKris <kristofferopdahl@mail.com>
2020-11-25 20:16:59 +0100

    init, Collection.kt

--- user-collections/src/main/kotlin/no/mock/exam/enterprise/model/Collection.kt
+++ user-collections/src/main/kotlin/no/mock/exam/enterprise/model/Collection.kt
@@ -2,6 +2,8 @@ package no.mock.exam.enterprise.model
 
 import no.mock.exam.enterprise.dto.CollectionDto
 import no.mock.exam.enterprise.dto.Hall
+import java.lang.IllegalArgumentException
+import kotlin.math.abs
 
 class Collection (
 
@@ -17,5 +19,21 @@ class Collection (
             dto.hallProbabilities.toMap()
     )
 
+    val moviesByHall : Map<Hall, List<Movie>> = movies.groupBy { it.hall }
+
+    init{
+        if(movies.isEmpty()){
+            throw IllegalArgumentException("No Cards")
+        }
+        Hall.values().forEach {
+            requireNotNull(prices[it])
+            requireNotNull(hallProbabilities[it])
+        }
+
+        val p = hallProbabilities.values.sum()
+        if(abs(1 - p) > 60.00000){
+            throw IllegalArgumentException("invalid probability sum $p")
+        }
+    }
 }
 
