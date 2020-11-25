e9db8dde08b2866206e458f9a86d2cb7e092bace
OpKris <kristofferopdahl@mail.com>
2020-11-25 11:44:54 +0100

    fixed pom user-collection

--- user-collections/src/main/kotlin/no/mock/exam/enterprise/MovieService.kt
+++ user-collections/src/main/kotlin/no/mock/exam/enterprise/MovieService.kt
@@ -1,6 +1,14 @@
 package no.mock.exam.enterprise
 
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
 class MovieService {
+
+    companion object{
+        private val logger = LoggerFactory.getLogger(MovieService::class.java)
+    }
 
 
 }
